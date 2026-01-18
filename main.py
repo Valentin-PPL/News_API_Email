d066229462d2fcb6f1f81f8d3497948f89c30648
gasto <gastonvalentino@gmail.com>
2026-01-18 19:49:45 +0200

    Good version

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-    # print(article["description"])
-    # print(article["title"])
-
-send_email(message=body)
-body = body.encode("utf-8")
-
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
-    if article["title"] is not None:
-for article in content["articles"]:
-body = ""
-# Access the article, titles and description
-
-content = req.json()
-# Get a dictionary with data
-
-req = requests.get(url)
-# Make request
-
-       "&apiKey=53357419416049389c34875c7def7bc5")
-       "&sortBy=publishedAt"
-       "&from=2025-12-18"
-       "q=tesla"
-url = ("https://newsapi.org/v2/everything?"
-api_key = "53357419416049389c34875c7def7bc5"
-
-from send_email import send_email
 import requests
+from send_email import send_email
+
+api_key = "53357419416049389c34875c7def7bc5"
+url = ("https://newsapi.org/v2/everything?"
+       "q=tesla"
+       "&from=2025-12-18"
+       "&sortBy=publishedAt"
+       "&apiKey=53357419416049389c34875c7def7bc5")
+
+# Make request
+req = requests.get(url)
+
+# Get a dictionary with data
+content = req.json()
+
+# Access the article, titles and description
+body = ""
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
+
+    # print(article["title"])
+    # print(article["description"])
